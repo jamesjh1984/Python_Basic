5b0dc0966aee673249d6bdf4cac35b543f38fc04
jamesjh1984 <24099235@qq.com>
2022-02-07 20:17:02 +0800

    Python Basic Demo
    Python connect to Oracle, MySQL, PostgreSQL.

---
+++ Basic_Demo/Demo11.py
@@ -0,0 +1,19 @@
+# input函数
+
+
+
+# name = input('your name:')
+# print(name, type(name))
+
+
+
+
+
+# 从键盘输入两个整数，计算他俩之和，并输出
+x1 = input('x1 = ')
+x2 = input('x2 = ')
+print('x1 + x2 =', (int(x1) + int(x2)))
+
+x1 = int(input('x1 = '))
+x2 = int(input('x2 = '))
+print('x1 + x2 =', (x1 + x2))
---
+++ Basic_Demo/Demo13.py
@@ -0,0 +1,42 @@
+# 赋值运算符
+
+
+a = b = c = 20
+print(a, id(a))
+print(b, id(b))
+print(c, id(c))
+
+
+
+a = 20
+a += 30
+print(a)
+
+a -= 10
+print(a)
+
+a *= 2
+print(a)
+
+a /= 3
+print(a)
+
+a //= 2
+print(a)
+
+
+
+
+print('-------- 解包赋值 --------')
+a, b ,c = 20, 30, 40
+print(a, b, c)
+
+
+
+
+print('-------- 交换两个变量的值 --------')
+a, b = 10, 20
+print('Before: ', a, b)
+
+a, b = b, a
+print('After: ', a, b)
