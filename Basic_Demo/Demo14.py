696aaf3dfecbf4f20020642810a841609b20b19c
jamesjh1984 <24099235@qq.com>
2022-02-10 10:47:02 +0800

    python

--- Basic_Demo/Demo14.py
+++ Basic_Demo/Demo14.py
@@ -1,42 +1,36 @@
 # 比较运算符
 
 
-a = b = c = 20
-print(a, id(a))
-print(b, id(b))
-print(c, id(c))
+
+
+print('-------- 比较运算符 --------')
+a, b = 20, 30
+print('a>b?', a>b)
+print('a<b?', a<b)
+print('a>=b?', a>=b)
+print('a<=b?', a<=b)
+print('a==b?', a==b)
+print('a!=b?', a!=b)
 
 
 
-a = 20
-a += 30
-print(a)
-
-a -= 10
-print(a)
-
-a *= 2
-print(a)
-
-a /= 3
-print(a)
-
-a //= 2
-print(a)
-
-
-
-
-print('-------- 解包赋值 --------')
-a, b ,c = 20, 30, 40
-print(a, b, c)
+print('-------- 标识比较运算符 数字 --------')
+a = 10
+b = 10
+print('a==b?', a==b) # value相等
+print('a is b?', a is b) # id标识(内存地址)相等
+print('a is not b?', a is not b) # id标识(内存地址)相等
+print('id(a): ', id(a))
+print('id(b): ', id(b))
 
 
 
 
-print('-------- 交换两个变量的值 --------')
-a, b = 10, 20
-print('Before: ', a, b)
-
-a, b = b, a
-print('After: ', a, b)
+print('-------- 标识比较运算符 数组 --------')
+list1 = [1,2,3,4]
+list2 = [1,2,3,4]
+print('list1==list2?', list1==list2) # value相等
+print('list1 is list2?', list1 is list2) # id标识(内存地址)不相等
+print('list1 is not list2?', list1 is not list2) # id标识(内存地址)不相等
+print('id(list1): ', id(list1))
+print('id(list2): ', id(list2))
